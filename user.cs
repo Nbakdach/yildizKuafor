662bbf1fbfa77b239dd5c2b5232cbc6973268630
Nbakdach <96494148+Nbakdach@users.noreply.github.com>
2024-12-18 12:47:20 +0300

    Create user.cs

---
+++ user.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace yidizKuafor.Models;
+
+public class user
+{ 
+    public int Id {get; set;}
+    public string Name {get; set ;}
+    public string Adress {get; set;}
+
+}
